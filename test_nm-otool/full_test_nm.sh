9b766915f6d181548a28bb55c838e73b1b62804d
ndudnicz <ndudnicz@student.42.fr>
2017-10-13 09:57:31 +0200

    del useless

--- test_nm-otool/full_test_nm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./ft_nm /usr/bin/* /bin/* /usr/lib/*.dylib > 1 ;
-nm /usr/bin/* /bin/* /usr/lib/*.dylib > 2 ;
-diff 1 2
--- test_nm-otool/full_test_otool.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./ft_otool /bin/* /usr/lib/*.dylib > 1 ;
-otool -t /bin/* /usr/lib/*.dylib > 2 ;
-diff 1 2
